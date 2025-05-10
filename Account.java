7323aff5b6c5339c6ee06fdb0219a2cc3cca3651
prakash-787 <145996356+prakash-787@users.noreply.github.com>
2025-05-10 20:40:52 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,3 +1,5 @@
+// Account.java
+
 public class Account {
     private String accountNumber;
     private String accountHolder;
@@ -7,5 +9,40 @@ public class Account {
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return accountNumber + "," + accountHolder + "," + balance;
     }
 }
